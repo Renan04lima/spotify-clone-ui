ba8bb784081627c5531b8ae7451b43d015c82da0
renan04lima <renan04lima@outlook.com>
2024-01-01 23:37:54 -0300

    feat: implement spotify content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as HomeIcon, Search, Library } from "lucide-react"
+import { Home as HomeIcon, Search, Library, ChevronRight, ChevronLeft, Play } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -37,7 +38,40 @@ export default function Home() {
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Late Night Vibes</a>
           </nav>
         </aside>
-        <main className="flex-1 p-6">main</main>
+        <main className="flex-1 p-6">
+          <div className='flex items-center  gap-3'>
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronLeft />
+            </button>
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa do álbum Dos prédios - Veigh" />
+              <strong>Dos prédios</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made For Renan Oliveira</h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <div className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa do álbum Dos prédios - Veigh" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </div>
+          </div>
+        </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
     </div>
